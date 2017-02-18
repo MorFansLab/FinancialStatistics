5e402d73442159e06e1315a8d02103149b61039c
SuperPaxxs <me@morfans.cn>
2017-02-18 11:24:40 +0800

    修改样式

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	// $(".mydetail").html('<i class="icon icon-spin icon-spinner"></i>加载中..');
+	// $(".detail").html('<i class="icon icon-spin icon-spinner"></i>加载中..');
+	
 	$.getJSON("./data.json",function(data){
 		var $table = $(".donate");
 		var $strHtml = "";
@@ -15,13 +18,16 @@ $(document).ready(function(){
 		})
 		$table.html($strHtml);
 		$(".sum").html(money);
+		$('table.datatable').datatable();
+		$('.fuck').datatable({sortable: true});
 		new $.zui.Messager('收到总赞助：￥'+money+'', {
-    type: 'success'
+    type: 'success' // 定义颜色主题
 }).show();
 	})
 
 $.getJSON("./mydata.json",function(data){
 		var $mytable = $(".mydetail");
+
 		var $mystrHtml = "";
 		var mymoney = "0.00";
 		$mytable.empty();
@@ -37,16 +43,36 @@ $.getJSON("./mydata.json",function(data){
 		$mytable.html($mystrHtml);
 		$(".mysum").html(mymoney);
 		new $.zui.Messager('支出总费用：￥'+mymoney+'', {
-    type: 'success'
+    type: 'success' // 定义颜色主题
 }).show();
 	});
 	new $.zui.Messager('感谢所有赞助我们的小伙伴们！', {
 		icon: 'heart',
-    type: 'primary',
+    type: 'primary', // 定义颜色主题
 		time: 0
 }).show();
+
+
+
+
+
+
 });
 
+
+
+
+Media = document.getElementById("video"); 
+Media.addEventListener('ended',function(){
+	$(".myvideo").css('filter','blur(90px)');
+	$(".myvideo").css('-webkit-filter','blur(90px)');
+	$(".tip").html('<p id="content">QQ:2685618862</p>');
+});
+Media.addEventListener('play',function(){
+	$(".myvideo").css('filter','blur(0px)');
+	$(".myvideo").css('-webkit-filter','blur(0px)');
+	$(".tip").html('');
+});
 
 
 
